16e47d5a0595d4d63f1c0c7675a8cdd13f5fcc57
hbjycl <370015769@qq.com>
2016-07-28 15:31:17 +0800

    还原jdbc

---
+++ src/main/java/StormKafkaTopo.java
@@ -0,0 +1,33 @@
+
+import com.hbjycl.bolt.BeforeBolt;
+import com.hbjycl.bolt.PersistentBolt;
+import org.apache.storm.Config;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.kafka.*;
+import org.apache.storm.spout.SchemeAsMultiScheme;
+import org.apache.storm.topology.TopologyBuilder;
+
+import java.util.UUID;
+
+public class StormKafkaTopo {
+
+    public static void main(String[] args) throws Exception {
+        TopologyBuilder builder = new TopologyBuilder();
+
+        BrokerHosts hosts = new ZkHosts("h1:2181,h2:2181,h3:2181");
+        SpoutConfig spoutConfig = new SpoutConfig(hosts, "jdbctopic", "/storm", UUID.randomUUID().toString());
+        spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
+        KafkaSpout kafkaSpout = new KafkaSpout(spoutConfig);
+        builder.setSpout("spout", kafkaSpout, 5);
+
+        builder.setBolt("beforebolt", new BeforeBolt(),1).shuffleGrouping("spout");
+
+        builder.setBolt("jdbcbolt", PersistentBolt.getJdbcInsertBolt(),2).shuffleGrouping("beforebolt");
+
+        Config config = new Config();
+
+        StormSubmitter.submitTopology(UUID.randomUUID().toString(), config, builder.createTopology());
+
+
+    }
+}
---
+++ src/main/java/com/hbjycl/bolt/BeforeBolt.java
@@ -0,0 +1,25 @@
+package com.hbjycl.bolt;
+
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+public class BeforeBolt extends BaseBasicBolt {
+
+
+    public void execute(Tuple input, BasicOutputCollector collector) {
+        String word = (String) input.getValue(0);
+        System.out.println("out=" + word);
+        collector.emit(new Values(word));
+    }
+
+
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        Fields outputFields = new Fields("content");
+
+        declarer.declare(outputFields);
+    }
+}
